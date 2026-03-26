3dd2020b8968e96cbf409009d1bf90abd50b88f7
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2026-03-26 14:22:30 +0530

    fix: update CORS origin to allow multiple domains and improve database connection error logging

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -1,9 +1,9 @@
 const mongoose=require("mongoose");
 const db=async ()=>{
     try{
-        await mongoose.connect(process.env.DB_URL || "mongodb://127.0.0.1:27017/foodi");
+        await mongoose.connect(process.env.DB_URL);
     }catch(err){
-      console.log("Error connecting to database",err);
+      console.log("yahi he",err);
     }
 }
 module.exports=db;
