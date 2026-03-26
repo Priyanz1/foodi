c46c47cb74d8aa310cec78a29861201ff111d184
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2026-03-26 15:07:17 +0530

    fix: update CORS origin configuration to use array format and improve server logging

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const db=require("./src/db/db");
 const server=async()=>{
    await db();
     app.listen(process.env.PORT,()=>{
+        console.log(`server is running on port ${process.env.PORT}`);
     })
 }
 
