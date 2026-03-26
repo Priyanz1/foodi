3f7d2fa7f37438522d436bd850a18c45075544f0
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2026-03-26 09:50:34 +0530

    fix: update CORS origin configuration for local development

--- backend/app.js
+++ backend/app.js
@@ -5,9 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin: process.env.NODE_ENV === "production" 
-      ? "https://foodi-ten-eta.vercel.app"
-      : ["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    origin:"http://localhost:5173",
     credentials: true
  }));
 
