155beea0d1112293ac69726a63ce85bfeb085d53
Priyanshu Nagina <naginapriyanshu83@gmail.com>
2026-03-26 10:39:43 +0530

    fix: update CORS origin to allow requests from production URL

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://foodi-khaki.vercel.app",
     credentials: true
  }));
 
